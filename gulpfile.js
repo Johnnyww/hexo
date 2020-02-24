cd550a12ba7a5654be846647aa5e1f58dcfc8db8
johnnychan <chenjunx@outlook.com>
2020-02-24 20:13:47 +0800

    删除压缩图片的代码

--- gulpfile.js
+++ gulpfile.js
@@ -37,15 +37,15 @@ gulp.task('minify-js', function() {
         .pipe(gulp.dest('./public'));
 });
 // 压缩图片
-gulp.task('minify-images', function() {
-    return gulp.src('./public/images/**/*.*')
-        .pipe(imagemin(
+//gulp.task('minify-images', function() {
+//    return gulp.src('./public/images/**/*.*')
+//        .pipe(imagemin(
 //        [imagemin.gifsicle({'optimizationLevel': 3}),
-        [imagemin.mozjpeg({'progressive': true}),
-        imagemin.optipng({'optimizationLevel': 7}),
-        imagemin.svgo()],
-        {'verbose': true}))
-        .pipe(gulp.dest('./public/images'));
-});
+//        [imagemin.mozjpeg({'progressive': true}),
+//        imagemin.optipng({'optimizationLevel': 7}),
+//        imagemin.svgo()],
+//        {'verbose': true}))
+//        .pipe(gulp.dest('./public/images'));
+//});
 // 默认任务
-gulp.task('default',gulp.series(gulp.parallel('minify-html','minify-css','minify-js','minify-images')));
+gulp.task('default',gulp.series(gulp.parallel('minify-html','minify-css','minify-js')));
